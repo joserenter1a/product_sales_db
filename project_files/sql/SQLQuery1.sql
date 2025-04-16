873457f4b82d6fdfff4714c94f206a6b7423ff4c
Jose Renteria <120423283+joserenter1a@users.noreply.github.com>
2025-04-15 20:15:05 -0700

    Updated SQL Query

--- project_files/sql/SQLQuery1.sql
+++ project_files/sql/SQLQuery1.sql
@@ -1,5 +1,29 @@
-select * from Product_data
+with cte as(
+select
 
-select * from product_sales
+a.Product,
+a.Category,
+a.Brand,
+a.Description,
+a.sale_price,
+a.cost_price,
+a.image_url,
+b.date,
+b.Customer_Type,
+b.discount_band,
+b.units_sold,
+(sale_price * Units_Sold) as revenue,
+(cost_price*Units_Sold) as total_cost,
+format(date,'MMM') as month,
+format(date,'yyyy') as year
+from Product_data a
+join product_sales b
+on a.Product_ID = b.Product)
 
-select * from discount_data
+
+select 
+*,
+1-discount*1.0/100
+from cte a
+join discount_data b
+on a.discount_band = b.discount_band and a.month = b.month
